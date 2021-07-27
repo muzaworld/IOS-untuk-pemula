11672b55ff9ae3f03d7bf47ace3c6e2b72e45b65
muzaworld <66401892+muzaworld@users.noreply.github.com>
2021-07-27 09:56:17 +0700

    Add files via upload

---
+++ MyHeroApp/MyHeroApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyHeroApp
+//
+//  Created by Muti Munika on 24/07/21.
+//  Copyright © 2021 Muza Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
