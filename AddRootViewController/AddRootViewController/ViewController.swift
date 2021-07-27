0d47a5aeeed30957727e8889744fd454e39d4b0c
muzaworld <66401892+muzaworld@users.noreply.github.com>
2021-07-27 09:50:51 +0700

    Add files via upload

---
+++ AddRootViewController/AddRootViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AddRootViewController
+//
+//  Created by Muti Munika on 23/07/21.
+//  Copyright © 2021 Muza Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
